f7091b12466e7108dfc5ac465c580f9a39572bbb
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-30 15:17:46 +0100

    remove sign

--- src/app/globals.js
+++ src/app/globals.js
@@ -39,9 +39,11 @@ const __Config__={
     setDefault:function(pairs){
         const keys=Object.keys(pairs);
         const values=Object.values(pairs);
-        for(let i=0;i<keys.length;i++)this.default[keys[i]]=values[i];
+        for(let i=0; i<keys.length; i++) this.default[keys[i]]=values[i];
     },
-    init:()=>document.documentElement.setAttribute("data-engine","zikojs"),
+    init:()=>{
+        // document.documentElement.setAttribute("data-engine","zikojs")
+    },
     renderingMode :"spa",
     isSSC : false,
 }
