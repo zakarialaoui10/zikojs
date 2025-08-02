3fcb7a34d06582caf930a9820a8b0b7dd02e5964
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-02 22:42:27 +0100

    fix semantic

--- src/ui/elements/semantic/index.js
+++ src/ui/elements/semantic/index.js
@@ -1,82 +1,42 @@
-import ZikoUIElement from "../ZikoUIElement";
-class ZikoUIMain extends ZikoUIElement{
-    constructor(){
-      super("main","Main");
+import ZikoUIElement from "../ZikoUIElement.js";
+const elements = ['Main', 'Header', 'Nav', 'Section', 'Article', 'Aside', 'Footer']
+
+// Storage for Classes and component functions
+const Classes = {};
+const Components = {};
+
+// Auto-generate Classes and factory functions
+for (let i=0; i<elements.length; i++) {
+  Classes[`ZikoUI${elements[i]}`] = class extends ZikoUIElement {
+    constructor() {
+      super(elements[i].toLowerCase());
+      this.style({ position: "relative" });
     }
-    get isMain(){
-      return true
+    get [`is${elements[i]}`]() {
+      return true;
     }
-  }
-  class ZikoUIHeader extends ZikoUIElement{
-    constructor(){
-      super("header","Header");
-    }
-    get isHeader(){
-      return true
-    }
-  }
-  class ZikoUINav extends ZikoUIElement{
-    constructor(){
-      super("nav","Nav");
-    }
-    get isNav(){
-      return true
-    }
-  }
-  class ZikoUISection extends ZikoUIElement{
-    constructor(){
-      super("section","Section");
-      this.style({position:"relative"})
-    }
-    get isSection(){
-      return true
-    }
-  }
-  class ZikoUIArticle extends ZikoUIElement{
-    constructor(){
-      super("article","Article");
-    }
-    get isArticle(){
-      return true
-    }
-  }
-  class ZikoUIAside extends ZikoUIElement{
-    constructor(){
-      super("aside","Aside");
-    }
-    get isAside(){
-      return true
-    }
-  }
-  class ZikoUIFooter extends ZikoUIElement{
-    constructor(){
-      super("footer","Footer");
-      this.element=document?.createElement("footer");
-    }
-    get isFooter(){
-      return true
-    }
-  }
-const Section = (...ZikoUIElement) => new ZikoUISection().append(...ZikoUIElement);
-const Article = (...ZikoUIElement) => new ZikoUIArticle().append(...ZikoUIElement);
-const Main = (...ZikoUIElement) => new ZikoUIMain().append(...ZikoUIElement);
-const Header = (...ZikoUIElement) => new ZikoUIHeader().append(...ZikoUIElement);
-const Footer = (...ZikoUIElement) => new ZikoUIFooter().append(...ZikoUIElement);
-const Nav = (...ZikoUIElement) => new ZikoUINav().append(...ZikoUIElement);
-const Aside = (...ZikoUIElement) => new ZikoUIAside().append(...ZikoUIElement);
-export{
-  Header,
+  };
+
+  Components[elements[i]] = (...children) =>
+    new Classes[`ZikoUI${elements[i]}`]().append(...children);
+}
+
+export const {
   Main,
+  Header,
+  Nav,
   Section,
   Article,
   Aside,
-  Nav,
-  Footer,
-  ZikoUIHeader,
+  Footer
+} = Components;
+
+export const {
   ZikoUIMain,
+  ZikoUIHeader,
+  ZikoUINav,
   ZikoUISection,
   ZikoUIArticle,
   ZikoUIAside,
-  ZikoUINav,
   ZikoUIFooter
-}
+} = Classes;
