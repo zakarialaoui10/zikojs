8105048a5e282091ebb22cbdd2e2bc1292c5beac
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-04 10:54:36 +0100

    remove jsonstyle

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,16 @@
 export * from "./ziko-app.js";
-export * from "./json-style-sheet.js";
 export * from "./spa.js"
 export * from "./params.js"
 export * from "./globals.js"
 export * from "./spa-file-based-routing.js"
 
 import * as __App__ from "./ziko-app.js"
-import * as JsonStyleSheet from "./json-style-sheet.js"
 import * as Spa from "./spa.js"
 import * as Global from "./globals.js"
 // import * as Params from "./params"
 
 const App={
     ...__App__,
-    ...JsonStyleSheet,
     ...Spa,
     ...Global,
     // ...Params
