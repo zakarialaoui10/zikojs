babde853a06c0ab8981af83f06c8f001c3350bdd
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-24 12:12:44 +0100

    add warns in define_wc

--- src/ui/web-component/index.js
+++ src/ui/web-component/index.js
@@ -1,4 +1,12 @@
-export function defineWC(name, UIElement, props = {}, { mode = 'open'} = {}) {
+export function define_wc(name, UIElement, props = {}, { mode = 'open'} = {}) {
+    if (globalThis.customElements?.get(name)) {
+        console.warn(`Custom element "${name}" is already defined`);
+        return; // skip redefinition
+    }
+    if(name.search('-') === -1){
+        console.warn(`"${name}" is not a valid custom element name`);
+        return; 
+    }
     globalThis.customElements?.define(
         name,
         class extends HTMLElement {
