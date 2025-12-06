6502398ae8507540877aee8b823a0a9d207c8f55
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-12-06 23:59:35 +0100

    add stats functions

---
+++ src/math/stats/index.js
@@ -0,0 +1,17 @@
+export * from './position/index.js';
+export * from './average/index.js';
+export * from './variability/index.js'
+export * from './rolling/index.js'
+export * from './accum/index.js'
+
+
+    // min
+// max
+
+// mean
+// variance
+// std
+// percentil 
+// accum
+
+//
